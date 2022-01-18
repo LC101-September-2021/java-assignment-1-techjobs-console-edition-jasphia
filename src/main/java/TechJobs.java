f14e4e6814f6406d76f6a40a6a27baafe8250d6b
jasphia <jaspanchana@gmail.com>
2022-01-17 19:46:25 -0500

    fixed errors part 1

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -122,7 +122,7 @@ public class TechJobs {
 
 //        System.out.println("printJobs is not implemented yet");
         if(someJobs.size() == 0){
-            System.out.println("No Results");
+            System.out.print("No Results");
         }else{
             for(HashMap<String, String> job : someJobs){
                 System.out.println("\n*****");
